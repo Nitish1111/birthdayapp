d0bbac6009240bf2eef07a6d2938f10258b11d8b
Nitish1111 <39581569+Nitish1111@users.noreply.github.com>
2019-09-16 18:35:59 +0530

    Delete adminRoutes.js

--- adminRoutes.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var router = express.Router();
-var employee = require('./employeeController');
-
-router.post('/insertEmployee',employee.insertEmployee);
-
-module.exports = router;
